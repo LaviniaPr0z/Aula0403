9e948b918087c2a88944a2db10bd3d879af12ffb
LaviniaPr0z <161752034+LaviniaPr0z@users.noreply.github.com>
2024-03-04 17:35:08 -0300

    Add files via upload

---
+++ AlunoT204_03/README.txt
@@ -0,0 +1,12 @@
+ex1.php é um exemplo para aprensentar o "Hello Mundo".
+ex2.php variável do tipo strings.
+ex3.php variável do tipo inteiro com condição if.
+ex4.php variável do tipo float com condição if.
+ex5.php variável do tipo Boolean com condição if.
+ex6.php variável de tipo array exemplo com "frutas".
+ex7.php variavel de objetos 
+ex8.php exemplos com echo e print
+ex9.php exemplos de funções ambiente
+ex10.php concatenação de strings
+ex11.php usamos gettype, settype e var_dump
+ex12.php usamos settype para apresentar divisão com valor inteiro 
---
+++ AlunoT204_03/ex7.php
@@ -0,0 +1,35 @@
+<?php
+// Definição da classe Pessoa
+class Pessoa {
+    public $nome;
+    public $idade;
+    
+    public function apresentar() {
+        return "Olá! Meu nome é {$this->nome} e tenho {$this->idade} anos.";
+    }
+}
+
+// Instanciando um objeto da classe Pessoa
+$pessoa = new Pessoa();
+$pessoa->nome = "Marcos";
+$pessoa->idade = 64;
+
+$pessoa2 = new Pessoa();
+$pessoa2 ->nome = "pedro";
+$pessoa2 ->idade = 24;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de Objeto com HTML</title>
+</head>
+<body>
+    <h1>Apresentação da Pessoa</h1>
+    <p><?php echo $pessoa->apresentar(); ?></p>
+
+    <p><?php echo $pessoa2->apresentar(); ?></p>
+</body>
+</html>
